de43da09eab40e50f289fd348ef7c213dcd82bda
Vojin <dvojinovic38@gmail.com>
2022-03-27 22:42:52 +0200

    Initial commit with assets

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Dusan Vojinovic on 27.3.22..
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Dusan Vojinovic on 27.3.22..
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
